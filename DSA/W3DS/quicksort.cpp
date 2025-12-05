a183625e66452ca752e25cceada1854dd1c07906
Lavish Kumar <lavish2008kumar@gmail.com>
2025-12-05 17:31:20 +0530

    Overload

--- DSA/W3DS/quicksort.cpp
+++ DSA/W3DS/quicksort.cpp
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(vector<int>& arr, int low, int high)
+int partition(vector<int> &arr, int low, int high)
 {
     int pivot = arr[0];
-    int i =low;
-    int j= high;
-        while (i < j)
+    int i = low;
+    int j = high;
+    while (i < j)
     {
-        while (arr[i] <= pivot && i<=high-1)
+        while (arr[i] <= pivot && i <= high - 1)
         {
             i++;
         }
-        while (arr[j]> pivot && j>= low+1){
+        while (arr[j] > pivot && j >= low + 1)
+        {
             j--;
         }
-        if (i<j){
+        if (i < j)
+        {
             swap(arr[i], arr[j]);
         }
     }
-    swap(arr[j],arr[low]);
+    swap(arr[j], arr[low]);
     return j;
 }
 
-int quicksort(vector<int>& arr, int low, int high)
+int quicksort(vector<int> &arr, int low, int high)
 {
     int pivot = 0;
     if (low < high)
@@ -39,8 +41,7 @@ int main()
     vector<int> arr = {10, 7, 8, 9, 1, 5};
     int n = arr.size();
     int res = 0;
-    quicksort(arr, 0, n-1);
-    cout << arr << endl;
+    quicksort(arr, 0, n - 1);
 
     return 0;
 }
