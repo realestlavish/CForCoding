712c7bca4e2fb4ac19f01662654e4a8f024d001f
Lavish Kumar <lavish2008kumar@gmail.com>
2025-12-10 00:12:30 +0530

    zeros to end optimalpluss

--- DSA/practice/zerostoend.cpp
+++ DSA/practice/zerostoend.cpp
@@ -34,11 +34,31 @@ void zerostoend(int arr[], int n)
     }
 }
 
+// Or
+
+void zerostoendoptimalplus(int arr[], int n)
+{
+    int pos = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            arr[pos] = arr[i];
+            pos++;
+        }
+    }
+    for (int i = n - pos; i < n; i++)
+    {
+        arr[i] = 0;
+    }
+}
+
 int main()
 {
 
     int arr[] = {1, 0, 2, 3, 0, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
     zerostoend(arr, n);
+    zerostoendoptimalplus(arr, n);
     return 0;
 }
