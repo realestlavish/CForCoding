be7183526c65498c0b0e343c0b3694fb1298de20
Lavish Kumar <lavish2008kumar@gmail.com>
2025-12-30 15:48:45 +0530

    DSA Restart DD1

--- DSA/practice/intersection.cpp
+++ DSA/practice/intersection.cpp
@@ -1,21 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void union2(int arr1[], int n1, int arr2[], int n2)
+void intersectioner(int arr1[], int n1, int arr2[], int n2)
 {
     map<int, int> mp1;
     map<int, int> mp2;
+    set<int> s;
+
     for (int i = 0; i < n1; i++)
     {
         mp1[arr1[i]]++;
     }
+
     for (int i = 0; i < n2; i++)
     {
         mp2[arr2[i]]++;
     }
 
+    for (auto it : mp1)
     {
-        /* code */
+        if (mp2.find(it.first) != mp2.end())
+        {
+            s.insert(it.first);
+        }
+    }
+
+    for (auto it : s)
+    {
+        cout << it << endl;
     }
 }
 
@@ -23,8 +35,10 @@ int main()
 {
     int arr1[] = {1, 0, 2, 3, 0, 4};
     int n1 = sizeof(arr1) / sizeof(arr1[0]);
-    int arr2[] = {2, 3, 5, 6};
+
+    int arr2[] = {2, 3, 5, 6, 0};
     int n2 = sizeof(arr2) / sizeof(arr2[0]);
-    union2(arr1, n1, arr2, n2);
+
+    intersectioner(arr1, n1, arr2, n2);
     return 0;
 }
