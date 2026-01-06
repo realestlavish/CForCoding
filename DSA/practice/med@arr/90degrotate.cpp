dba6c9696d5dd805f964c9d3e01d1347e0aa4f27
Lavish <lavish2008kumar@gmail.com>
2026-01-06 13:25:38 +0530

    Condition Correction

--- DSA/practice/med@arr/90degrotate.cpp
+++ DSA/practice/med@arr/90degrotate.cpp
@@ -9,18 +9,25 @@ void rotater(vector<vector<int>> &matrix)
     {
         for (int j = i + 1; j < m; j++)
         {
-            if (i != j)
             {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
         reverse(matrix[i].begin(), matrix[i].end());
     }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 int main()
 {
 
-    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {19, 10, 11, 12}, {13, 14, 15, 16}};
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
     rotater(matrix);
 
     return 0;
