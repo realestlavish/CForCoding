0c11d0ad4bcdadfa886c22084d59ddf889c9db08
Lavish Kumar <lavish2008kumar@gmail.com>
2026-01-15 16:02:20 +0530

    UpperBound BS Day 17

---
+++ DSA/BinarySearch/Upperbound.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Lowerbound(vector<int> arr, int n, int target)
+{
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] > target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> arr = {2, 4, 5, 6, 9, 10, 12};
+    int n = arr.size();
+    int target = 10;
+    cout << "Lower Bound of " << target << " is at index: " << Lowerbound(arr, n, target) << endl;
+    return 0;
+}
