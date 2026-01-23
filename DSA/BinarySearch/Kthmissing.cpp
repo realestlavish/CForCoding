009b0fac6a13832cbefc5e816b44af8ce4c5801a
Lavish Kumar <lavish2008kumar@gmail.com>
2026-01-24 01:07:19 +0530

    BS Day 25

---
+++ DSA/BinarySearch/Kthmissing.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingbinary(vector<int> &vec, int k, int n)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int missing = vec[mid] - vec[0] - mid; 
+
+        if (missing < k)
+        {
+            low = mid + 1; 
+        }
+        else high = mid - 1; 
+    }
+    return low + k;
+}
+//we use low+k or we can use high+1+k both are same, low and high go to opposite polarity, ans = vec[high]+more, more = k-(missing till high).
+
+int main()
+{
+    vector<int> vec = {4, 7, 9, 10};
+    int k = 4;
+    int n = vec.size();
+
+    // Call the function
+    int result = missingbinary(vec, k, n);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
