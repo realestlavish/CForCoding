da3b728e7ac78ded465f69f3013770ee4dcb43f6
Lavish Kumar <lavish2008kumar@gmail.com>
2026-02-25 00:00:50 +0530

    LL dAY 54

---
+++ DSA/LinkedList/med-LL/Add1.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+class Sorter
+{
+public:
+    /*T.C -> TLE*/
+    static Node *LinkedListTraversing(vector<int> arr)
+    {
+        Node *head = new Node(arr[0]);
+        Node *mover = head;
+
+        for (int i = 1; i < arr.size(); i++)
+        {
+            Node *temp = new Node(arr[i]);
+            mover->next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+    static Node *OneBruteAdder(Node *head)
+    {
+        if (head == NULL)
+            return NULL;
+        Node *temp = head;
+        stack<int> st;
+        long long number = 0;
+        while (temp != NULL)
+        {
+            number = number * 10 + temp->data;
+            temp = temp->next;
+        }
+        number += 1;
+        while (number != 0)
+        {
+            int rem = number % 10;
+            st.push(rem);
+            number = number / 10;
+        }
+        int headwiser = st.top();
+        st.pop();
+        Node *newhead = new Node(headwiser);
+        Node *mover = newhead;
+        while (!st.empty())
+        {
+            Node *temp = new Node(st.top());
+            st.pop();
+            mover->next = temp;
+            mover = temp;
+        }
+        return newhead;
+    }
+
+    static Node *RecursiveReverser(Node *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+        Node *newHead = RecursiveReverser(head->next);
+        Node *front = head->next;
+        front->next = head;
+        head->next = NULL;
+
+        return newHead;
+    }
+    /*T.C -> (3N)*/
+    static Node *TwoBruteAdder(Node *head)
+    {
+        if (head == NULL)
+            return NULL;
+        int carry = 1;
+        Node *newhead = RecursiveReverser(head);
+        Node *temphead = newhead;
+        while (temphead != NULL)
+        {
+            temphead->data = temphead->data + carry;
+            if (temphead->data < 10)
+            {
+                carry = 0;
+                break;
+            }
+            else
+            {
+                temphead->data = 0;
+                carry = 1;
+            }
+            temphead = temphead->next;
+        }
+        if (carry == 1)
+        {
+            Node *newNode = new Node(1);
+            head = RecursiveReverser(newhead);
+            newNode->next = head;
+            return newNode;
+        }
+        return RecursiveReverser(newhead);
+    }
+    // Recursive function to add one from least significant digit (rightmost node)
+    int addOneUtil(Node *node)
+    {
+        // Base case: when reaching beyond last node, return carry = 1
+        if (!node)
+            return 1;
+
+        // Recurse to the end
+        int carry = addOneUtil(node->next);
+        int sum = node->data + carry;
+        node->data = sum % 10;
+        // Return new carry
+        return sum / 10;
+    }
+
+    // Function to add one to the number represented by the linked list
+    Node *addOne(Node *head)
+    {
+        // Perform recursive addition
+        int carry = addOneUtil(head);
+
+        // If carry remains after processing the head, create a new head node
+        if (carry)
+        {
+            Node *newHead = new Node(carry);
+            newHead->next = head;
+            head = newHead;
+        }
+
+        return head;
+    }
+};
+}
+}
+;
+int main()
+{
+    Node *head = nullptr;
+    LinkedList ll;
+    Solution sol;
+
+    // Example: Number 129 (1 -> 2 -> 9)
+    head = ll.append(head, 1);
+    head = ll.append(head, 2);
+    head = ll.append(head, 9);
+
+    cout << "Original Number: ";
+    ll.printList(head);
+
+    head = sol.addOne(head);
+
+    cout << "After Adding One: ";
+    ll.printList(head);
+
+    return 0;
+}
