ceab646ad28448007c0f0f66ab50dfcde720aa25
Lavish <lavish2008kumar@gmail.com>
2026-08-03 00:00:02 +0530

    stack d3

--- DSA/Stack_Queue/CustomStack.cpp
+++ DSA/Stack_Queue/CustomStack.cpp
@@ -37,9 +37,93 @@ public:
     }
 };
 
+// Class to implement Minimum Stack
+class MinStack {
+private:
+    stack <int> st;
+    int mini;
+    
+public:
+    
+    MinStack() {
+    }
+    
+    void push(int value) {
+        
+        // If stack is empty
+        if(st.empty()) {
+            //Update the minimum value
+            mini = value;
+            
+            // Push current value as minimum
+            st.push( value );
+            return;
+        }
+        
+        // If the value is greater than the minimum
+        if(value > mini) {
+            st.push(value);
+        }
+        else {
+            // If the value is smaller than the minimum
+            // Add the modified value to stack
+            st.push(2 * value - mini);
+            // Update the minimum
+            mini = value;
+        }
+    }
+
+    void pop() {
+        if(st.empty()) return;
+        
+        // Get the top
+        int x = st.top();
+        st.pop(); // Pop operation
+        
+        // If the modified value was added to stack
+        if(x < mini) {
+            // Update the minimum
+            // rollback to previous minimum
+            mini = 2 * mini - x;
+        }
+    }
+    
+    int top() {
+        if(st.empty()) return -1;
+        
+        int x = st.top();
+        // Returnn top if minimum is less than the top (it is not a modified value)
+        if(mini < x) return x;
+        
+        //Otherwise return mini
+        return mini;
+    }
+    
+    int getMin() {
+        // Return the minimum
+        return mini;
+    }
+};
+
+int main() {
+    MinStack s;
+    
+    // Function calls
+    s.push(-2);
+    s.push(0);
+    s.push(-3);
+    cout << s.getMin() << " ";
+    s.pop();
+    cout << s.top() << " ";
+    s.pop();
+    cout << s.getMin();
+    
+    return 0;
+}
+
 int main()
 {
-    CustomStack2 s;
+    MinStack s;
 
     // Function calls
     s.push(-2);
