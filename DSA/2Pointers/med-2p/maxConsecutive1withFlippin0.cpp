bc60ed33d87925d99ca5faabf695a7c26d607c9f
Lavish <lavish2008kumar@gmail.com>
2026-09-13 23:14:12 +0530

    2pointer day2

---
+++ DSA/2Pointers/med-2p/maxConsecutive1withFlippin0.cpp
@@ -0,0 +1,30 @@
+#include <unordered_map>
+using namespace std;
+
+int maxConsecutiveOnes(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    int left = 0;
+    int right = 0;
+    int maxCount = 0;
+    int zeroCount = 0;
+    int cnt = 0;
+    while (right < n)
+    {
+        if (arr[right] == 0)
+        {
+            zeroCount++;
+        }
+        while (zeroCount > k)
+        {
+            if (arr[left] == 0)
+            {
+                zeroCount--;
+            }
+            left++;
+        }
+        maxCount = max(maxCount, right - left + 1);
+        right++;
+    }
+    return maxCount;
+}
